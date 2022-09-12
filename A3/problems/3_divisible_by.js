0f8ffa87ab01d825df4cafed16517d015e8766b4
Mlkz <maho.mamulashvili@gmail.com>
2022-09-12 18:30:34 -0400

    add divisible by

--- A3/problems/3_divisible_by.js
+++ A3/problems/3_divisible_by.js
@@ -10,7 +10,20 @@ divisibleByFivePairSum([13, 22, 8, -3, 12]); // => [[ 0, 1 ], [ 0, 3 ], [ 0, 4 ]
 
 function divisibleByFivePairSum(array){
   // your code here...
+  let allPairs = [];
 
+  for (let i = 0; i < array.length - 1; i++) { // length - 1 is necessary to avoid adding next (which doesn't exists)
+    for (let j = i + 1; j < array.length; j++) {  // to the last element
+  
+      let tempSum = array[i] + array[j];
+
+      if (tempSum % 5 === 0) {
+        allPairs.push([i, j]);
+      }
+    }
+  }
+
+  return allPairs;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/

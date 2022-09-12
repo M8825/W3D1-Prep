ae5b61eab87d388e6b44ad5dcc09752f9be1aebb
Mlkz <maho.mamulashvili@gmail.com>
2022-09-12 19:12:49 -0400

    add hipsterfy

--- A4/problems/5_hipsterfy.js
+++ A4/problems/5_hipsterfy.js
@@ -13,8 +13,26 @@ hipsterfy("turtle cheeseburger fries"); // => "turtl cheeseburgr fris"
 
 function hipsterfy(sentence) {
   // your code here...
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let arrSent = sentence.split(' ');
+  let newSent = []
 
+  for (let i = 0; i < arrSent.length; i++) {
+    let tempWord = arrSent[i].split('');
+
+    for (let i = tempWord.length - 1; i >= 0; i--) {
+      if (vowels.includes(tempWord[i])) {
+        tempWord.splice(i, 1);
+        break;
+      }
+    }
+
+    newSent.push(tempWord.join(''));
+  }
+
+  return newSent.join(' ');
 }
 
+hipsterfy("proper tonic panther"); // => "propr tonc panthr"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
 module.exports = hipsterfy;
